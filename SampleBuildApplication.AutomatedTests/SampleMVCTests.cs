57ede45f282db848530fd3d4f61e18bca2b9f50e
Balaji GK <Balaji.GK@marlabs.com>
2016-07-05 15:39:55 +0530

    Added selenium automation scripts

---
+++ SampleBuildApplication.AutomatedTests/SampleMVCTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using Selenium;
+
+namespace SampleBuildApplication.AutomatedTests
+{
+    [TestFixture]
+    public class SampleMVCTests
+    {
+        private ISelenium selenium;
+        private StringBuilder verificationErrors;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            selenium = new DefaultSelenium("localhost", 4444, "*firefox", "http://localhost:54892/");
+            selenium.Start();
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                selenium.Stop();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheSampleMVCTestsTest()
+        {
+            selenium.Open("/");
+            selenium.Click("link=Home");
+            selenium.WaitForPageToLoad("30000");
+            selenium.Click("link=About");
+            selenium.WaitForPageToLoad("30000");
+            selenium.Click("link=Contact");
+            selenium.WaitForPageToLoad("30000");
+            selenium.Click("link=Home");
+            selenium.WaitForPageToLoad("30000");
+        }
+    }
+}
+
