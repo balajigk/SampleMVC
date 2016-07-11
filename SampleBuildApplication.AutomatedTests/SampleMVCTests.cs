44ac05e6379f87ea55c612a72b562a16b3febfa7
Balaji GK <Balaji.GK@marlabs.com>
2016-07-11 15:54:01 +0530

    port no changed in selenium tests

--- SampleBuildApplication.AutomatedTests/SampleMVCTests.cs
+++ SampleBuildApplication.AutomatedTests/SampleMVCTests.cs
@@ -16,7 +16,7 @@ namespace SampleBuildApplication.AutomatedTests
         [SetUp]
         public void SetupTest()
         {
-            selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://localhost:54892/");
+            selenium = new DefaultSelenium("localhost", 8888, "*firefox", "http://localhost:54892/");
             selenium.Start();
             verificationErrors = new StringBuilder();
         }
